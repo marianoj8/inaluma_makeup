1bd4e99b36590d0c8753edafa8c2c570d0e021dd
Mariano JavaSwing <marianogoncalves8@gmail.com>
2023-12-11 20:06:26 +0100

    initial commit

---
+++ src/main/java/dev/marianoj8/inaluma/InalumaApplication.java
@@ -0,0 +1,13 @@
+package dev.marianoj8.inaluma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InalumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InalumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marianoj8/inaluma/InalumaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marianoj8.inaluma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InalumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
