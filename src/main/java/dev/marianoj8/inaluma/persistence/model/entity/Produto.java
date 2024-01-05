5dae4c97e58e759cc9fc7f213ef29f438ae47460
Mariano JavaSwing <marianogoncalves8@gmail.com>
2024-01-05 08:32:52 +0100

    Produto class model has been refactored

--- src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Produto.java
+++ src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Produto.java
@@ -2,6 +2,7 @@ package dev.marianoj8.inaluma.persistence.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,8 +26,16 @@ public class Produto extends CustomAbstractEntity {
     private int stock;
     private String code;
 
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] data;
+
+    private String fileName;
+    private String contentType;
+    private double fixedSize;
+
     public Produto(Long id, LocalDateTime createdAt, LocalDateTime lastModifiedAt, int totalModified, String nome,
-                   String descricao, String tipo, boolean estado, double preco, int stock, String code) {
+                   String descricao, String tipo, boolean estado, double preco, int stock, String code, byte[] data, String fileName, String contentType, double fixedSize) {
         super(id, createdAt, lastModifiedAt, totalModified);
         this.nome = nome;
         this.descricao = descricao;
@@ -35,5 +44,10 @@ public class Produto extends CustomAbstractEntity {
         this.preco = preco;
         this.stock = stock;
         this.code = code;
+        
+        this.data = data;
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.fixedSize = fixedSize;
     }
 }
