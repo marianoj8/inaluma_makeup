322b25772ff9b81e57735207ad1aa45bb471d328
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-18 00:03:32 +0100

    refactored Agendamento entity

--- src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Agendamento.java
+++ src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Agendamento.java
@@ -1,7 +1,8 @@
 package dev.marianoj8.inaluma.persistence.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotNull;
@@ -10,7 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -19,12 +20,18 @@ import dev.marianoj8.inaluma.persistence.model.entity.utils.AbstractDocument;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Entity
 public class Agendamento extends AbstractDocument {
-  @NotNull @Temporal(TemporalType.DATE)
-  private LocalDate dataAgendada;
+  @NotNull @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime dataHoraAgendada;
 
-  @OneToOne(targetEntity = User.class) @OnDelete(action = OnDeleteAction.RESTRICT)
+  @ManyToOne(targetEntity = Servico.class, optional = false) @OnDelete(action = OnDeleteAction.RESTRICT)
+  private Servico servico;
+
+  @ManyToOne(targetEntity = User.class) @OnDelete(action = OnDeleteAction.RESTRICT)
   private User funcionario;
 
-  @OneToOne(mappedBy = "agendamento") @OnDelete(action = OnDeleteAction.CASCADE)
-  private Factura factura;
+  @Column(columnDefinition = "Bit(1) default 0")
+  private Boolean reagendado;
+
+  @ManyToOne(targetEntity = Agendamento.class) @OnDelete(action = OnDeleteAction.RESTRICT)
+  private Agendamento agendamentoOriginal;
 }
