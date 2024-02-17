4c1f155af6641bdf5acd97e9d7a7bff740eac5f3
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-17 04:07:18 +0100

    added PagamentoService to base hierarchy

--- src/main/java/dev/marianoj8/inaluma/persistence/service/PagamentoService.java
+++ src/main/java/dev/marianoj8/inaluma/persistence/service/PagamentoService.java
@@ -3,20 +3,17 @@ package dev.marianoj8.inaluma.persistence.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import dev.marianoj8.inaluma.persistence.model.entity.Pagamento;
 import dev.marianoj8.inaluma.persistence.repository.PagamentoRepository;
+import dev.marianoj8.inaluma.persistence.service.util.BaseService;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-
-@Service @AllArgsConstructor
-public class PagamentoService {
-  @Autowired private PagamentoRepository repository;
-
-  public Pagamento findById(Long id) { return repository.findById(id).get(); }
-  public List<Pagamento> findAll() { return repository.findAll(); }
-  public Pagamento create(Pagamento dto) { return repository.save(dto); }
-  public Pagamento update(Pagamento dto) { return repository.save(dto); }
-  public void delete(Long id) { repository.deleteById(id); }
+@Service @NoArgsConstructor
+public class PagamentoService extends BaseService<Pagamento, PagamentoRepository> {
+  public List<Pagamento> fetchByFacturaId(@NonNull Long id) { return repository.fetchByFacturaId(id); }
+  public List<Pagamento> fetchByOperadorId(@NonNull Long id) { return repository.fetchByOperadorId(id); }
 }
