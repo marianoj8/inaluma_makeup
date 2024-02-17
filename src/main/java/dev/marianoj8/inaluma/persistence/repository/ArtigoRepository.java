8bbeb18abeeb30336803c4aad4983599c4d30ace
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-17 11:41:15 +0100

    added ArtigoRepository to base hierarchy

--- src/main/java/dev/marianoj8/inaluma/persistence/repository/ArtigoRepository.java
+++ src/main/java/dev/marianoj8/inaluma/persistence/repository/ArtigoRepository.java
@@ -2,17 +2,20 @@ package dev.marianoj8.inaluma.persistence.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.marianoj8.inaluma.persistence.model.entity.Artigo;
+import dev.marianoj8.inaluma.persistence.repository.util.BaseRepository;
 
 @Repository
-public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
-  @Query("select a from Artigo a where a.tipo.nome = ?1")
-  public Artigo getByTipo(String nomeTipo);
+public interface ArtigoRepository extends BaseRepository<Artigo> {
+  @Query("select a from Artigo a where a.tipo.id = ?1")
+  public List<Artigo> fetchByTipoId(Long id);
   
   @Query("select f from Artigo f where f.tipo.isProduto = true")
   public List<Artigo> fetchProdutos();
+  
+  @Query("select f from Artigo f where f.tipo.isProduto = false")
+  public List<Artigo> fetchServicos();
 }
