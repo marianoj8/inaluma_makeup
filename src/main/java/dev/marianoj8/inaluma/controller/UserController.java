b266d5cedc78307896bac2491eb04c93224766ec
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-17 12:03:24 +0100

    added UserController to inserts base hierarchy

--- src/main/java/dev/marianoj8/inaluma/controller/UserController.java
+++ src/main/java/dev/marianoj8/inaluma/controller/UserController.java
@@ -2,7 +2,6 @@ package dev.marianoj8.inaluma.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,15 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.marianoj8.inaluma.controller.util.BaseControllerWithInserts;
 import dev.marianoj8.inaluma.persistence.model.entity.User;
 import dev.marianoj8.inaluma.persistence.model.entity.UserRole;
 import dev.marianoj8.inaluma.persistence.service.UserService;
-import lombok.AllArgsConstructor;
 
-@RestController @AllArgsConstructor @RequestMapping("v1/users")
-public class UserController {
-  @Autowired UserService service;
-
+@RestController @RequestMapping("v1/users")
+public class UserController extends BaseControllerWithInserts<User, UserService> {
   @GetMapping("fetchBy/role")
   public ResponseEntity<List<User>> getByRole(@RequestBody UserRole role) { return ResponseEntity.ok(service.fetchByRole(role)); }
 
