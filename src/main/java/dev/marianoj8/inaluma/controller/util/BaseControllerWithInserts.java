ad7852317a4af9b1dff59e1db912ea623cb5c717
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-17 03:47:31 +0100

    created a base controller with create and update mappings

---
+++ src/main/java/dev/marianoj8/inaluma/controller/util/BaseControllerWithInserts.java
@@ -0,0 +1,20 @@
+package dev.marianoj8.inaluma.controller.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import dev.marianoj8.inaluma.persistence.model.entity.utils.CustomAbstractEntity;
+import dev.marianoj8.inaluma.persistence.service.util.BaseService;
+import jakarta.persistence.MappedSuperclass;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass @NoArgsConstructor
+public class BaseControllerWithInserts<T extends CustomAbstractEntity, S extends BaseService<T, ?>> extends BaseController<T, S>{
+  public ResponseEntity<T> create(@NonNull @RequestBody T dto) { return new ResponseEntity<T>(service.create(dto), HttpStatus.CREATED); }
+
+  @PutMapping("update")
+  public ResponseEntity<T> update(@NonNull @RequestBody T dto) { return new ResponseEntity<T>(service.update(dto), HttpStatus.ACCEPTED); }
+}
