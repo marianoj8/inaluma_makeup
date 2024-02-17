d1b8db849e1ba7d604b2fbbe5d7203c6594bf076
Piedoso Pedro <piedosopedro@gmail.com>
2024-02-17 21:32:32 +0100

    refactored Factura entity

--- src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Factura.java
+++ src/main/java/dev/marianoj8/inaluma/persistence/model/entity/Factura.java
@@ -1,16 +1,11 @@
 package dev.marianoj8.inaluma.persistence.model.entity;
 
-import java.util.List;
-import java.util.Set;
-
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import dev.marianoj8.inaluma.persistence.model.entity.utils.AbstractDocument;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,12 +13,9 @@ import lombok.Setter;
 
 @Entity @Getter @Setter @AllArgsConstructor
 public class Factura extends AbstractDocument {
-  @OneToOne(targetEntity = Agendamento.class, optional = true) @OnDelete(action = OnDeleteAction.RESTRICT) 
+  @OneToOne(targetEntity = Agendamento.class) @OnDelete(action = OnDeleteAction.RESTRICT) 
   private Agendamento agendamento;
 
-  @OneToMany(mappedBy = "factura")
-  private final Set<Pagamento> pagamentos;
-
-  @OneToMany(mappedBy = "factura")
-  private final List<ItemFactura> itens;
+  @ManyToOne(targetEntity = TipoCompra.class, optional = false) @OnDelete(action = OnDeleteAction.RESTRICT)
+  private TipoCompra tipoCompra;
 }
